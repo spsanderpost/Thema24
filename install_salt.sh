80230a410a5ce21a525a034a21d22942a4e4bce1
Sander <sa.post@st.hanze.nl>
2018-06-28 22:31:52 +0200

    bash

--- install_salt.sh
+++ install_salt.sh
@@ -2,8 +2,8 @@
 
 apt install salt-api salt-cloud salt-master salt-ssh salt-syndic -y
 
-service salt-master start
-
 cd /etc/salt
 cat file_roots.yaml >> master
 cd /
+
+service salt-master restart
